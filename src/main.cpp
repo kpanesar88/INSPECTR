89f2a60483cb9704a5e9bca0c64aebaa32a74ee2
Karanveer Panesar <Karanveerpanesar04@gmail.com>
2026-01-11 17:18:25 -0500

    auto refresh values

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 #include <iomanip>
+#include <thread>
+#include <chrono>
 
 #include <monitor/cpu.hpp>
 #include <monitor/memory.hpp>
 #include <monitor/systeminfo.hpp>
 
 int main() {
-    // Snapshot APIs (v1.1)
-    CpuInfo cpu     = getCpuInfo();
-    MemoryInfo mem  = getMemoryInfo();
-    SystemInfo sys  = getSystemInfo();
+
+    constexpr int REFRESH_MS = 1000;
 
     std::cout << std::fixed << std::setprecision(2);
 
-    std::cout << "=== SYSTEM BUDDY ===\n";
+    while (true) {
+        system("cls");
 
-    // ---------------- CPU ----------------
-    std::cout << "\n---------- CPU ----------\n";
-    std::cout << "Name            : " << cpu.name << "\n";
-    std::cout << "Usage           : " << cpu.usage_percent << " %\n";
-    std::cout << "Base Frequency  : " << cpu.base_freq_ghz << " GHz\n";
-    std::cout << "Cores           : " << cpu.cores << "\n";
-    std::cout << "Threads         : " << cpu.threads << "\n";
+        CpuInfo cpu     = getCpuInfo();
+        MemoryInfo mem  = getMemoryInfo();
+        SystemInfo sys  = getSystemInfo();
 
-    // ---------------- MEMORY ----------------
-    double totalMemGB = mem.total_bytes / (1024.0 * 1024.0 * 1024.0);
-    double usedMemGB  = mem.used_bytes  / (1024.0 * 1024.0 * 1024.0);
+        std::cout << "=== SYSTEM BUDDY (v2.0) ===\n";
 
-    std::cout << "\n---------- MEMORY ----------\n";
-    std::cout << "Total           : " << totalMemGB << " GB\n";
-    std::cout << "Used            : " << usedMemGB << " GB\n";
-    std::cout << "Usage           : " << mem.usage_percent << " %\n";
+        // ---------------- CPU ----------------
+        std::cout << "\n---------- CPU ----------\n";
+        std::cout << "Name            : " << cpu.name << "\n";
+        std::cout << "Usage           : " << cpu.usage_percent << " %\n";
+        std::cout << "Base Frequency  : " << cpu.base_freq_ghz << " GHz\n";
+        std::cout << "Cores           : " << cpu.cores << "\n";
+        std::cout << "Threads         : " << cpu.threads << "\n";
 
-    // ---------------- SYSTEM ----------------
-    std::cout << "\n---------- SYSTEM ----------\n";
-    std::cout << "OS              : " << sys.os << "\n";
-    std::cout << "Uptime          : " << sys.uptime << "\n";
+        // ---------------- MEMORY ----------------
+        double totalMemGB = mem.total_bytes / (1024.0 * 1024.0 * 1024.0);
+        double usedMemGB  = mem.used_bytes  / (1024.0 * 1024.0 * 1024.0);
+
+        std::cout << "\n---------- MEMORY ----------\n";
+        std::cout << "Total           : " << totalMemGB << " GB\n";
+        std::cout << "Used            : " << usedMemGB << " GB\n";
+        std::cout << "Usage           : " << mem.usage_percent << " %\n";
+
+        // ---------------- SYSTEM ----------------
+        std::cout << "\n---------- SYSTEM ----------\n";
+        std::cout << "OS              : " << sys.os << "\n";
+        std::cout << "Uptime          : " << sys.uptime << "\n";
+
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds(REFRESH_MS)
+        );
+    }
 
     return 0;
 }
